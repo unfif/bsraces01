cfd4c8a1e0a3c0b9783e56dc975823c31b10bcac
undo5 <undo5@hotmail.co.jp>
2018-05-16 22:10:12 +0900

    template 01

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 import os
 from datetime import datetime
 
@@ -6,14 +6,25 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    html = '<header style="padding: 20px; background: #000; color: #fff;"><h1>Test app 01</h1></header>'
-    html += '<div class="section">' + datetime.now().ctime() + '</div>'
-    html += '<div class="section">' + os.getcwd() + '</div>'
-    for each in os.environ:
-        html += '<div class="section">' + each + '</div>'
-        
-    html += '<style>body, h1, h2, h3, h4, h5, h6{margin: 0; padding: 0;} .section{padding: 20px; background: #eee; border-bottom: 2px solid #000;}</style>'
-    return html
+    data{
+        'ctime': datetime.now().ctime(),
+        'cwd': os.getcwd(),
+        'env': os.environ
+    }
+    # html = '<header style="padding: 20px; background: #000; color: #fff;"><h1>Test app 01</h1></header>'
+    # html += '<div class="section">' + datetime.now().ctime() + '</div>'
+    # html += '<div class="section">' + os.getcwd() + '</div>'
+    # for each in os.environ:
+    #     html += '<div class="section">' + each + '</div>'
+    #
+    # html += '<style>body, h1, h2, h3, h4, h5, h6{margin: 0; padding: 0;} .section{padding: 5px 20px; background: #eee; border-bottom: 2px solid #000;}</style>'
+    # return html
+
+    return render_template('index.html', data=data)
+
+@app.route("/favicon.ico")
+def favicon():
+    return app.send_static_file("favicon.ico")
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', '5000'))
