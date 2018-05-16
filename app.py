da6b10f04fc1ce2e16a9c59017e94e4812b06397
undo5 <undo5@hotmail.co.jp>
2018-05-16 21:00:41 +0900

    fix 01

--- app.py
+++ app.py
@@ -7,8 +7,9 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     html = '<header style="padding: 20px; background: #000; color: #fff;"><h1>Test app 01</h1></header>'
-    html += '<div class="contents" style="padding: 20px; background: #eee;">' + datetime.now().ctime() + '</div>'
-    html += '<style>body, h1, h2, h3, h4, h5, h6{margin: 0; padding: 0;}</style>'
+    html += '<div class="section"">' + datetime.now().ctime() + '</div>'
+    html += '<div class="section" style="padding: 20px; background: #eee; border-bottom: 2px solid #000;">' + os.cwd() + '</div>'
+    html += '<style>body, h1, h2, h3, h4, h5, h6{margin: 0; padding: 0;} .section{padding: 20px; background: #eee; border-bottom: 2px solid #000;}</style>'
     return html
 
 if __name__ == '__main__':
